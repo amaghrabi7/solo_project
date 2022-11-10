5149b88b598834e1169b66a20341d6a61f227142
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-11-10 11:24:45 +0300

    added Movie and Watchlist classes

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Watchlist(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    movie = models.ManyToManyField(
+        Movie, related_name='watchlists'
+    )
+    is_watched = models.BooleanField(default=False)
